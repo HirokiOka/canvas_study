84f28bfaa07de222addaf48bba2677613d918842
hiroki <hiro_bass.0827@icloud.com>
2020-03-02 15:10:34 +0900

    implement player

--- canvas2D/shooting/script/script.js
+++ canvas2D/shooting/script/script.js
@@ -1,4 +1,6 @@
 (() => {
+    window.isKeyDown = {};
+
     const CANVAS_WIDTH = 640;
     const CANVAS_HEIGHT = 480;
 
@@ -6,6 +8,8 @@
     let canvas = null;
     let ctx = null;
     let image = null;
+    let startTime = null;
+    let viper = null;
 
     window.addEventListener('load', () => {
         util = new Canvas2DUtility(document.body.querySelector('#main_canvas'));
@@ -15,6 +19,8 @@
         util.imageLoader('./image/viper.png', (loadImage) => {
             image = loadImage;
             initialize();
+            eventSetting();
+            startTime = Date.now();
             render();
         });
     }, false);
@@ -22,11 +28,35 @@
     function initialize() {
         canvas.width = CANVAS_WIDTH;
         canvas.height = CANVAS_HEIGHT;
+
+        viper = new Viper(ctx, 0, 0, 64, 64, image);
+        viper.setComing(
+            CANVAS_WIDTH / 2,
+            CANVAS_HEIGHT + 50,
+            CANVAS_WIDTH / 2,
+            CANVAS_HEIGHT - 100
+        );
+    }
+
+    function eventSetting() {
+        
+        window.addEventListener('keydown', (event) => {
+            isKeyDown[`key_${event.key}`] = true;
+        }, false);
+
+        window.addEventListener('keyup', (event) => {
+            isKeyDown[`key_${event.key}`] = false;
+        }, false);
     }
 
     function render() {
+
+        ctx.globalAlpha = 1.0;
         util.drawRect(0, 0, canvas.width, canvas.height, '#eeeeee');
-        ctx.drawImage(image, 100, 100);
+        let nowTime = (Date.now() - startTime) / 1000;
+
+        viper.update();
+        requestAnimationFrame(render);
     }
 
     function generateRandomInt(range) {
